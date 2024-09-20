1c79690c83d44c4498dfa1416b01cd7a8a5d3772
sunmeng <meng.sun@tomosight.com>
2024-09-20 16:41:21 +0800

    0920

--- debugger_view/src/Filter_Elevation.cpp
+++ debugger_view/src/Filter_Elevation.cpp
@@ -1,12 +1,8 @@
 ﻿#include <ImGuiCommon.h>
 
-int main(int argc, char* argv[])
+int main()
 {
-    vtkNew<vtkRenderer> ren;
-    vtkNew<vtkRenderWindow> renWin;
-    renWin->AddRenderer(ren);
-    vtkNew<vtkRenderWindowInteractor> iren;
-    iren->SetRenderWindow(renWin);
+    BEFORE_MY_CODE
 
     // vtkns::labelWorldZero(ren);
 
@@ -35,49 +31,13 @@ int main(int argc, char* argv[])
 
     ren->ResetCamera();
 
-    ::pWindow = renWin;
     ::imgui_render_callback = [&]
         {
             vtkns::vtkObjSetup("elevation", elevationF, ImGuiTreeNodeFlags_DefaultOpen);
-            vtkns::vtkObjSetup("src", src);
+            vtkns::vtkObjSetup("SphereSource", src);
             vtkns::vtkObjSetup("actor", actor);
             vtkns::vtkObjSetup("bar", bar);
         };
 
-    // Start rendering app
-    ren->SetBackground(0., 0., 0.);
-    renWin->Render(); // 非常重要！！
-
-    /// Change to your code begins here. ///
-    // Initialize an overlay with DearImgui elements.
-    vtkNew<vtkDearImGuiInjector> dearImGuiOverlay;
-    // 💉 the overlay.
-    dearImGuiOverlay->Inject(iren);
-    // These functions add callbacks to ImGuiSetupEvent and ImGuiDrawEvents.
-    vtkns::SetupUI(dearImGuiOverlay);
-    // You can draw custom user interface elements using ImGui:: namespace.
-    vtkns::DrawUI(dearImGuiOverlay);
-    /// Change to your code ends here. ///
-
-    vtkNew<vtkCameraOrientationWidget> camManipulator;
-    camManipulator->SetParentRenderer(ren);
-    camManipulator->On();
-    auto rep = vtkCameraOrientationRepresentation::SafeDownCast(camManipulator->GetRepresentation());
-    rep->AnchorToLowerRight();
-
-    // Start event loop
-#if 0
-    renderWindow->SetSize(1920, 1000);
-#else
-#ifdef _WIN32
-    // 获取窗口句柄
-    HWND hwnd = ::FindWindow(NULL, renWin->GetWindowName());
-    // 最大化窗口
-    ::ShowWindow(hwnd, SW_MAXIMIZE);
-#endif
-#endif
-    vtkInteractorStyleSwitch::SafeDownCast(iren->GetInteractorStyle())->SetCurrentStyleToTrackballCamera();
-    iren->EnableRenderOff();
-    iren->Start();
-    return 0;
+    AFTER_MY_CODE
 }
